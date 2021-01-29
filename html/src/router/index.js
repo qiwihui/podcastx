5bb57d88643fd7163c52c868cc13b5aa2336b528
qiwihui <qwh005007@gmail.com>
2021-01-29 17:44:57 +0800

    feat: maybe it's a embed audio

--- html/src/router/index.js
+++ html/src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Home from '@/views/Home'
 import Podcast from '@/views/Podcast'
+import EmbedPodcast from '@/views/EmbedPodcast'
 
 Vue.use(Router)
 
@@ -17,6 +18,11 @@ export default new Router({
       path: '/podcasts/:podcastId',
       name: 'Podcast',
       component: Podcast
+    },
+    {
+      path: '/embed/:podcastId',
+      name: 'EmbedPodcast',
+      component: EmbedPodcast
     }
   ]
 })
---
+++ html/src/views/EmbedPodcast.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="container">
+    <div class="audio-player">
+      <podcast-player :articleId="articleId"></podcast-player>
+    </div>
+  </div>
+</template>
+
+<script>
+import NavBar from '@/components/NavBar'
+import PodcastPlayer from '@/components/PodcastPlayer'
+export default {
+  name: 'EmbedPodcast',
+  data () {
+    return {
+      error_message: '',
+      loading: false,
+      articleId: this.$route.params.podcastId
+    }
+  },
+  mounted () {
+  },
+  methods: {
+  },
+  components: {
+    NavBar,
+    PodcastPlayer
+  }
+}
+</script>
+
+<style scoped>
+
+.container {
+  /* max-width: 40rem; */
+  margin-left: auto;
+  margin-right: auto;
+  /* margin-bottom: 50px; */
+  padding: 0 15px;
+}
+
+a {
+  color: #42b983;
+}
+
+.audio-player {
+  width: 100%;
+  max-width: 40rem;
+  /* margin: 0 auto 30px auto; */
+}
+
+</style>
